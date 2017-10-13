932460be2d99e358ce5214c73514b3832ab5aa29
rivermont <william.11bennett@gmail.com>
2017-10-12 22:20:10 -0400

    Updated get.py

--- get.py
+++ get.py
@@ -4,7 +4,13 @@ Copyright (c) Will Bennett/rivermont 2017.
 
 # import requests
 # import markovify
+import time as t
+
+date = t.time()
 
 # Get list of today's articles (from reddit?) from sites that can be parsed.
 # Download the articles - not the entire page, just the article content.
 # Save all of the text to a file.
+
+with open('{0}.txt', 'w+') as file:
+    pass
